afd1ecbe71ca71c25cb59098f9afd8ee9bac3fbe
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2021-09-17 14:51:06 +0200

    Fik skabt en måde at skabe en vare.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,45 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface IProduct {
+    productId: number
+    productNr: number
+    customerNr: number
+    invoiceNr: number
+    serialNr: string
+}
+
+// use https (http secure).
+// http (non secure) will make the app complain about mixed content when running the app from Azure
+let baseUrl: string = "https://customerrestservice.azurewebsites.net/api/Products"
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        addData: { productNr: "", customerNr: "", invoiceNr: "", serialNr: ""},
+        addMessage: "",
+    },
+    created() {
+        let params = new URLSearchParams(location.search);
+        this.addData.customerNr = params.get('customerNr')
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        addProduct() {
+            axios.post<IProduct>(baseUrl, this.addData)
+                .then((response: AxiosResponse) => {
+                    
+                    let message: string = "Skabningen af ny vare er " + response.statusText
+                    this.addMessage = message
+                })
+                .catch((error: AxiosError) => {
+                    // this.addMessage = error.message
+                    alert(error.message)
+                })
         }
     }
 })
